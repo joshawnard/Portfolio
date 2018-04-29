289c99e62e340caf5e6c33d95d3aee7a03cd8dfd
Joshua Ard <ard.josh@gmail.com>
2018-04-29 16:55:25 -0400

    implemented concern for managing image data

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
+  include Placeholder
+
   after_initialize :set_defaults
 
   validates_presence_of :title, :body, :main_image, :thumb_image
@@ -10,7 +12,7 @@ class Project < ApplicationRecord
   end
 
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: 400, width: 600)
+    self.thumb_image ||= Placeholder.image_generator(height: 200, width: 350)
   end
 end
