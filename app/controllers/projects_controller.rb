90ab7db6f5e9cb8e303f250e8b38f77e13ff8af6
Joshua Ard <ard.josh@gmail.com>
2018-04-28 20:54:56 -0400

    add show action for Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,10 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
   def edit
     @project = Project.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :projects
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
