951b3f791c7a9485e8609bcf1d37c55aa2b28c90
Joshua Ard <ard.josh@gmail.com>
2018-04-28 20:17:59 -0400

    add new and create actions for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,26 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: "Project was successfully created." }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:title, :subtitle, :body)
+    end
 end
