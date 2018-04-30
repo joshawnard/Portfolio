189064f969b65a8ca350e348f1215ea36ba78b8b
Joshua Ard <ard.josh@gmail.com>
2018-04-29 21:11:01 -0400

    add first and last name methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
