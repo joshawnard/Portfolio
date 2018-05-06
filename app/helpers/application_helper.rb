7850b39ad8d1ac94beba1619f10329b5d329bfc8
Joshua Ard <ard.josh@gmail.com>
2018-05-06 19:31:26 -0400

    ActionView helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
       (link_to "Log in", new_user_session_path, method: :get)
     end
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag :div, greeting, class: "source-helper"
+    end
+  end
 end
